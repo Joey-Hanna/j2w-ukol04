a168ca80bcd3144c89771c358316ca64e37697d0
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-10-26 14:41:33 +0200

    vizitky se zobrazuji spravne a tlacitka 'Smazat' funguji - jenom po vyplneni formulare a redirect na hlavni strance se zobrazi prazdna vizitka.

--- src/main/java/cz/czechitas/java2webapps/ukol3/service/VizitkaService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/service/VizitkaService.java
@@ -79,16 +79,6 @@ public class VizitkaService {
         );
     }
 
-/*public class VizitkaService {
-    private final List<Vizitka> seznamVizitek = List.of(
-            new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s", "Václavské náměstí 837/11", "11000 Praha 1", "dita@czechitas.cs", " +420 800123456", " www.czechitas.cz"),
-            new Vizitka("Barbora Bühnová", "Czechitas z. s", "Václavské náměstí 837/11", "11000 Praha 1", "", " +420 800123456", " www.czechitas.cz"),
-            new Vizitka("Monika Ptáčníková", "Czechitas z. s", "Václavské náměstí 837/11", "11000 Praha 1", "monika@czechitas.cs", " +420 800123456", " www.czechitas.cz"),
-            new Vizitka("Mirka Zatloukalová", "Czechitas z. s", "Václavské náměstí 837/11", "11000 Praha 1", "mirka@czechitas.cs", "",  " www.czechitas.cz"),
-            new Vizitka("Ondřej Čejka", "Czechitas z. s.", "Jungmannova 3", "77900 Olomouc", "", "", "www.czechitas.cz"),
-            new Vizitka("Kateřina Reiglová", "Czechitas z. s.", "Lipová 1789/9", "37005 České Budějovice", "", "", "www.czechitas.cz"  )
-    );*/
-
     public List<Vizitka> getAll() {
         return seznamVizitek;
     }
